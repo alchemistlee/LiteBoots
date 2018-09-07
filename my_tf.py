86d77012f65b1755b883052ea5ed58e12d2cdd44
alchemistlee <alchemistlee@alchemistleedeMacBook-Pro.local>
2018-09-07 11:49:23 +0800

    up

--- my_tf.py
+++ my_tf.py
@@ -123,7 +123,9 @@ def index():
   return render_template('index.html')
 
 if __name__ == '__main__':
+  app.debug = True
   handler = logging.FileHandler("/home/yifan/anywhere/logs/my-tf-flask.log",encoding="UTF-8")
+  handler.setLevel(logging.DEBUG)
   logging_format = logging.Formatter('%(asctime)s - %(levelname)s - %(filename)s - %(funcName)s - %(lineno)s - %(message)s')
   handler.setFormatter(logging_format)
   app.logger.addHandler(handler)
