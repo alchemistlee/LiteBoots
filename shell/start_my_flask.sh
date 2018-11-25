719a9ff11719877fcab0ba9dcdb03b9703dee89d
alchemistlee <alchemistlee@alchemistleedeMacBook-Pro.local>
2018-11-25 18:34:26 +0800

    up

--- shell/start_my_flask.sh
+++ shell/start_my_flask.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+sh /home/root/start-enzh.sh
+echo 'start enzh ! ...'
+sleep 6s
+
+
+sh /home/root/start-zhen.sh
+echo 'start zhen ! ...'
+sleep 5s
+
 nohup python3 /home/root/LiteBoots/my_tf.py &
 
 echo "start tf_flask !"
