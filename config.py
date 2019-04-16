69c54e2f9687957a88c0fe45d284f092648b1ded
alchemistlee <liyifan1988>
2019-04-16 09:52:35 +0800

    add is_replace

--- config.py
+++ config.py
@@ -8,7 +8,7 @@
 EN2ZH_GET_ALL='select id,ent_keys,ent_val,type,is_replace from en_zh_ent where is_delete=0;'
 EN2ZH_GET_MAX='select update_time from en_zh_ent order by update_time desc limit 1;'
 
-ZH2EN_GET_ALL='select id,ent_keys,ent_val,type from zh_en_ent where is_delete=0;'
+ZH2EN_GET_ALL='select id,ent_keys,ent_val,type,is_replace from zh_en_ent where is_delete=0;'
 ZH2EN_GET_MAX='select update_time from zh_en_ent order by update_time desc limit 1;'
 
 EN2ZH_DEL='delete from en_zh_ent where id=%s;'
